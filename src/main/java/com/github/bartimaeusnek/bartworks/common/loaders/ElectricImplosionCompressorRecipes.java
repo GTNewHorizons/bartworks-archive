9dc0491aff583762e39befac1054c71e30deae15
GTNH-Colen <54497873+GTNH-Colen@users.noreply.github.com>
2023-02-17 13:20:29 +0000

    Restore recipe for optical chips

--- src/main/java/com/github/bartimaeusnek/bartworks/common/loaders/ElectricImplosionCompressorRecipes.java
+++ src/main/java/com/github/bartimaeusnek/bartworks/common/loaders/ElectricImplosionCompressorRecipes.java
@@ -48,6 +48,22 @@ public class ElectricImplosionCompressorRecipes implements Runnable {
                     // Recipe stats.
                     100 * 20,
                     (int) TierEU.RECIPE_UMV);
+
+            // Raw Exposed Optical Chip
+            eicMap.addRecipe(
+                false,
+                new ItemStack[] {
+                    ItemList.Circuit_Silicon_Wafer7.get(1L),
+                    getModItem("universalsingularities", "universal.general.singularity", 1L, 20)
+                },
+                new ItemStack[] {ItemList.Circuit_Chip_Optical.get(16L)},
+                null,
+                new FluidStack[] {GT_Values.NF},
+                new FluidStack[] {GT_Values.NF},
+                5 * 20,
+                (int) TierEU.RECIPE_UMV,
+                1);
+
         }
 
         addElectricImplosionRecipe(

cc251126a03b59cc7147e9f15de326c3e7b610f5
boubou19 <miisterunknown@gmail.com>
2023-03-12 12:55:53 +0100

    reduce muffler hatches to 1

--- src/main/java/com/github/bartimaeusnek/bartworks/common/tileentities/multis/mega/GT_TileEntity_MegaBlastFurnace.java
+++ src/main/java/com/github/bartimaeusnek/bartworks/common/tileentities/multis/mega/GT_TileEntity_MegaBlastFurnace.java
@@ -114,12 +114,12 @@ public class GT_TileEntity_MegaBlastFurnace extends GT_TileEntity_MegaMultiBlock
 
         raw[0] = new String[15];
         String topCasing = "ttttttttttttttt";
-        String mufflerLine = "tmmmmmmmmmmmmmt";
+        String middleTopCasing = "tttttttmttttttt";
         raw[0][0] = topCasing;
-        for (int i = 1; i < 14; i++) {
-            raw[0][i] = mufflerLine;
+        for (int i = 1; i < 15; i++) {
+            raw[0][i] = topCasing;
         }
-        raw[0][14] = topCasing;
+        raw[0][7] = middleTopCasing;
 
         raw[1] = new String[15];
         String allGlass = "ggggggggggggggg";
